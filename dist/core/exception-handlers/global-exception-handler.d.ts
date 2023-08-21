bdf70f9fc3ac01ed84c0637d0fdb969c19129106
Ashish Bhatnagar <63673042+ashishBhatnagar01@users.noreply.github.com>
2023-08-21 23:18:57 +0530

    Delete dist directory

--- dist/core/exception-handlers/global-exception-handler.d.ts
+++
@@ -1,4 +0,0 @@
-import { ArgumentsHost, ExceptionFilter } from "@nestjs/common";
-export declare class GlobalExceptionHandler implements ExceptionFilter {
-    catch(exception: any, host: ArgumentsHost): any;
-}
